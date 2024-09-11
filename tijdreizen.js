699d931d20c6514537c35608b85de6b07404f9c6
AT-STjemoeder <lucaskokken06@gmail.com>
2024-09-11 16:53:25 +0200

    Create tijdreizen.js

---
+++ tijdreizen.js
@@ -0,0 +1,8 @@
+const date = new Date(1955, 8, 12, 10, 39, 5, 10);    
+console.log(date);
+const date2 = new Date(1985, 6, 12, 10, 39, 5, 10);
+console.log(date2)
+const date3 = new Date(2015, 7, 12, 10, 39, 5, 10);
+console.log(date3)
+const date4 = new Date(1884, 12, 12, 10, 39, 5, 10);
+console.log(date4)
