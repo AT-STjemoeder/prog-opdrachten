3cbeabfa25a580b03272b8a61cb39f7f59077a3c
AT-STjemoeder <lucaskokken06@gmail.com>
2024-09-11 17:16:16 +0200

    Create playerstats.js

---
+++ playerstats.js
@@ -0,0 +1,22 @@
+let name = "Erwin Henraat";
+console.log("Name: " + name);
+let jumpforce = "20";
+console.log("jumpforce: " + jumpforce);
+let specialpower = "teleport";
+console.log("specialpower: " + specialpower);
+let speed = "280";
+console.log("speed: " + speed);
+let bike = "Honda VFR VTEC";
+console.log("bike: " + bike);
+let lives = "1";
+console.log("lives left: " + lives);
+let ammo = "121"
+console.log("ammo: " + ammo);
+let grenades = "5";
+console.log("grenades left: " + grenades);
+let energy = "100%";
+console.log("energy: " + energy);
+let level = "8000";
+console.log("level: " + level);
+let wanted = "five stars";
+console.log("wanted level: " + wanted);
