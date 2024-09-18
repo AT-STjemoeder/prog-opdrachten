7173db0655efe44f136106e4436a256ff1197f3f
AT-STjemoeder <lucaskokken06@gmail.com>
2024-09-18 15:26:31 +0200

    moet naar scorro

---
+++ moet ik naar school.js
@@ -0,0 +1,12 @@
+const huidigeDag= "zaterdag";
+const schooldag1= "maandag";
+const schooldag2= "dinsdag";
+const schooldag3= "woendag";
+const schooldag4= "donderdag";
+const schooldag5= "vrijdag";
+
+if(huidigeDag == schooldag1 || huidigeDag== schooldag2 || huidigeDag== schooldag3 || huidigeDag== schooldag4 || huidigeDag== schooldag5){
+    console.log("Ik moet naar school");
+} else {
+    console.log("Lekker uitslapen!");
+}
