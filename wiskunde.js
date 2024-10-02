d9dedd1812ba30589a39539a0e718e4d280915e2
lucas <37939@ma-web.nl>
2024-10-02 16:37:12 +0200

    wiskunde verbetering

--- wiskunde.js
+++ wiskunde.js
@@ -1,28 +1,27 @@
 function blokje(lengte, hoogte, diepte){
-   return lengte * hoogte * diepte
+   return lengte * hoogte * diepte;
 }
 let blokjeInhoud = blokje(10, 5, 10);
 console.log(blokjeInhoud);
 
-function inhoudblokje(diameter, hoogte1){
-   let pi = Math.PI
-   return diameter * pi * hoogte1
+function inhoudbuis(diameter, hoogte1){
+   let pi = Math.PI;
+   let straal = diameter / 2;
+   return Math.pow(straal,2) * pi * hoogte1
 }
 
-let inhoudK = inhoudblokje(10, 5);
-console.log(inhoudK); 
+let inhoudB = inhoudbuis(5, 5);
+console.log(inhoudB); 
 
 function driehoek(lengte2, hoogte2){
-   let l2 = Math.pow(lengte2,2)
-   return Math.pow(hoogte2,2) + l2
+   return Math.sqrt(Math.pow(lengte2, 2) + Math.pow(hoogte2, 2));
 }
-
-let drie = driehoek(10,20);
+let drie = driehoek(20,50);
 console.log(drie)
 
 function nummers(one,two,three,four,five,six,seven){
-   let gemmidelde = one + two + three + four + five + six + seven
-   return gemmidelde / 7
+   let gemmidelde = one + two + three + four + five + six + seven;
+   return gemmidelde / 7;
 }
 
 let gemmidelde = nummers(2,6,4,3,7,2,5)
